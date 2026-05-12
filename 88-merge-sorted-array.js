c61f150dc3e268b10287ee129c438831ec18ce5b
Aleksandar Duncevic <aleksandarduncevic@gmail.com>
2026-05-12 20:10:41 +0200

    solve: 88. Merge Sorted Array (Easy) optimize space complexity

--- 88-merge-sorted-array.js
+++ 88-merge-sorted-array.js
@@ -1,3 +1,31 @@
+// Time Complexity O(m + n)
+// Space Complexity O(1)
+// Start merging from the end of nums1
+
+var merge = function(nums1, m, nums2, n) {
+    let i = m - 1; // End of the first array
+    let j = n - 1; // End of the second array
+    let k = m + n - 1; // End of the merged array
+
+    while (i >= 0 && j >= 0) {
+        if (nums2[j] > nums1[i]) {
+            nums1[k--] = nums2[j--];
+        } else {
+            nums1[k--] = nums1[i--];
+        }
+    }
+    
+    // If there are any remaining elements in nums2
+    while (j >= 0) {
+        nums1[k--] = nums2[j--];
+    }
+};
+
+
+/*
+// Time Complexity O(m + n)
+// Space Complexity O(m + n)
+
 var merge = function(nums1, m, nums2, n) {
     const result = [];
     let i = 0;
@@ -19,3 +47,5 @@ var merge = function(nums1, m, nums2, n) {
         nums1[i] = result[i];
     }
 };
+
+*/
