1704f3f323326496a204ee82e15ea900c03a0666
Aleksandar Duncevic <aleksandarduncevic@gmail.com>
2026-05-18 18:57:09 +0200

    solve: 189. Rotate Array (Medium) optimize space complexity

--- 189-rotate-array.js
+++ 189-rotate-array.js
@@ -1,5 +1,40 @@
+// Time Complexity O(n)
+// Space Complexity O(1)
+
+var rotate = function(nums, k) {
+    k %= nums.length;
+
+    // Reverse the nums
+    let i = 0;
+    let j = nums.length - 1;
+    swap(i, j);
+    
+    // Reverse elements from 0 to k-1
+    i = 0;
+    j = k - 1;
+    swap(i, j);
+
+    // Reverse elements from k to nums.length - 1
+    i = k;
+    j = nums.length - 1;
+    swap(i, j);
+
+    function swap(i, j) {
+        while (i < j) {
+            let temp = nums[j];
+            nums[j] = nums[i];
+            nums[i] = temp;
+            i++;
+            j--;
+        }
+    }
+};
+
+
+/*
 // Time complexity O(n)
 // Space complexity O(n)
+
 var rotate = function(nums, k) {
     k %= nums.length;
     const copy = [];
@@ -10,3 +45,4 @@ var rotate = function(nums, k) {
     nums.length = 0;
     nums.push(...copy);
 };
+*/
